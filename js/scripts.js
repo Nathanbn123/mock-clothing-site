cbe82431fde48a580f81635c659a7eedd541381d
Nathan Nielson and Randee Layosa <randee@>
2019-01-16 16:51:35 -0800

    Add radio button values to receipt.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
   $("#orderForm").submit(function(event) {
     var customerInput = $("input#customer").val();
     var addressInput = $("input#address").val();
-    var flavor = $("input:radio[name=size1]:checked").val();
-    var flavor = $("input:radio[name=size2]:checked").val();
-    var flavor = $("input:radio[name=size3]:checked").val();
+    var shirtInput = $("input:radio[name=shirt]:checked").val();
+    var cargoInput = $("input:radio[name=cargo]:checked").val();
+    var slideInput = $("input:radio[name=slide]:checked").val();
 
     $(".customer").text(customerInput);
     $(".address").text(addressInput);
+    $("#shirt").text(shirtInput);
+    $("#cargo").text(cargoInput);
+    $("#slide").text(slideInput);
+
 
     $("#orderForm").toggle();
     $("#receipt").fadeToggle();
