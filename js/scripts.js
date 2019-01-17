0256155dba3f0ed258250eb88531bb33cf1c238f
Nathan Nielson and Randee Layosa <nathan@>
2019-01-16 16:23:44 -0800

    Add content. Style with CSS, JS, and jQuery. Add radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
   $("#orderForm").submit(function(event) {
     var customerInput = $("input#customer").val();
     var addressInput = $("input#address").val();
+    var flavor = $("input:radio[name=size1]:checked").val();
+    var flavor = $("input:radio[name=size2]:checked").val();
+    var flavor = $("input:radio[name=size3]:checked").val();
 
     $(".customer").text(customerInput);
     $(".address").text(addressInput);
 
-    $("#receipt").show();
+    $("#orderForm").toggle();
+    $("#receipt").fadeToggle();
 
     event.preventDefault();
   });
